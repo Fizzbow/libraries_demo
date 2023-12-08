2f1b28ae4f7f315273bf9e83668dee8e8737fc34
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2023-12-08 14:09:21 +0800

    tailwindcss init

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const langs = ["en", "cn"];
 
 function App() {
   const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    console.log(t("title"));
-  }, [i18n.language]);
-
   return (
-    <div>
-      <h1>Vite + React</h1>
-      <div className="card ">
+    <div className="app flex flex-row">
+      <div className="app-content flex flex-col w-full">
+        <nav className="flex w-full flex-row-reverse">
+          <button>open nav</button>{" "}
+        </nav>
+        <h1 className="text-red-50">{i18n.language}</h1>
+        <p>{t("title")}</p>
+      </div>
+
+      <div className="app-nav flex flex-col bg-slate-700 h-full w-[7rem]">
+        {" "}
         {langs.map((lang) => {
           return (
             <button key={lang} onClick={() => i18n.changeLanguage(lang)}>
@@ -22,10 +24,6 @@ function App() {
             </button>
           );
         })}
-
-        <h1>{i18n.language}</h1>
-
-        <p>{t("title")}</p>
       </div>
     </div>
   );
